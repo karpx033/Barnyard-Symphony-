3cbbf1445f81a1a3d15397d8f537edf5b0c7e7e5
Juno Nguyen <junonguyen27@gmail.com>
2022-03-03 15:24:03 -0600

    working on login and signup routes, server and client

--- server/seeds/seed.js
+++ server/seeds/seed.js
@@ -1,12 +1,15 @@
 const db = require('../config/connection');
-const { songString } = require('../models');
+const { songString, Profile } = require('../models');
 
 const songData = require('./songData.json');
+const profileSeeds = require('./profileSeeds.json');
 
 db.once('open', async () => {
   await songString.deleteMany({});
+  await Profile.deleteMany({});
 
   const technologies = await songString.insertMany(songData);
+  const profiles = await Profile.insertMany(profileSeeds);
 
   console.log('Technologies seeded!');
   process.exit(0);
