1d4b00b69258ab2178318fd4c26118c803ee36da
Juno Nguyen <junonguyen27@gmail.com>
2022-03-04 10:28:59 -0600

    moved links to only show up when signed in

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -24,6 +24,12 @@ const Header = () => {
               <button className="btn btn-lg btn-light m-2" onClick={logout}>
                 Logout
               </button>
+              <Link className="btn btn-lg btn-light m-2" to="/animals">
+                Animals
+              </Link>
+              <Link className="btn btn-lg btn-light m-2" to="/play">
+                Play
+              </Link>
             </>
           ) : (
             <>
@@ -32,12 +38,6 @@ const Header = () => {
               </Link>
               <Link className="btn btn-lg btn-light m-2" to="/signup">
                 Signup
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/animals">
-                Animals
-              </Link>
-              <Link className="btn btn-lg btn-light m-2" to="/play">
-                Play
               </Link>
             </>
           )}
