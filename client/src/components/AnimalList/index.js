5ed096602736b8491c5415b20506d5aa31c99114
Louis <karpx033@umn.edu>
2022-03-07 17:15:58 -0600

    spaced carousel to better fit background image

--- client/src/components/AnimalList/index.js
+++ client/src/components/AnimalList/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import cowDemo from '../../media/CowDemo.mp4';
 import chickenDemo from '../../media/ChickenDemo.mp4';
 import sheepDemo from '../../media/SheepDemo.mp4';
-import '../../styles/animals.css';
 import Carousel from 'react-bootstrap/Carousel';
+import '../../styles/animals.css';
 
 var cowNoise = function () {
   var cowSounds = document.getElementById("cowSound");
@@ -37,30 +37,32 @@ const AnimalList = ({ songStrings, title }) => {
   }
 
   return (
-    <div className="animalsbackground">
-      <Carousel variant="dark">
+    <div className="animalsbackground heightening">
+      <Carousel interval="8000000" className="caroBox" variant="dark">
         {songStrings &&
           songStrings.map((songString) => (
 
             <Carousel.Item>
-              <img className="card-img-top" src={songString.img} alt="test"></img>
-              <Carousel.Caption>
-                <h5 className="card-title">{songString.name}</h5>
-                <p className="card-text">{songString.description}</p>
+
+              <img className="smallIMG" src={songString.img} alt="test"></img>
+              <h5 className='centerTxt'>{songString.name}</h5>
+              <p className='centerTxt'>{songString.description}</p>
+              <div className='btnDiv'>
                 {songString.name === "Cow" &&
-                  <button type="button" class="btn btn-primary" onClick={cowNoise} style={buttonSpace}>Play Sound</button>
+                  <button type="button" className="btn animalbtn btn-primary" onClick={cowNoise} style={buttonSpace}>Play Sound</button>
                 }
                 {songString.name === "Chicken" &&
-                  <button type="button" class="btn btn-primary" onClick={chickenNoise} style={buttonSpace}>Play Sound</button>
+                  <button type="button" className="btn animalbtn btn-primary" onClick={chickenNoise} style={buttonSpace}>Play Sound</button>
                 }
                 {songString.name === "Sheep" &&
-                  <button type="button" class="btn btn-primary" onClick={sheepNoise} style={buttonSpace}>Play Sound</button>
+                  <button type="button" className="btn animal btn btn-primary" onClick={sheepNoise} style={buttonSpace}>Play Sound</button>
                 }
                 <button type="button" className="btn btn-primary" style={buttonSpace}>Add to Song</button>
-                <audio src={cowDemo} id="cowSound"> Your browser does not support the <code>audio</code> element.</audio>
+              </div>
+              <audio src={cowDemo} id="cowSound"> Your browser does not support the <code>audio</code> element.</audio>
               <audio src={chickenDemo} id="chickenSound"> Your browser does not support the <code>audio</code> element.</audio>
               <audio src={sheepDemo} id="sheepSound"> Your browser does not support the <code>audio</code> element.</audio>
-              </Carousel.Caption>
+
             </Carousel.Item>
           ))}
       </Carousel>
