cae93ad2fd1203692c6d3aa716801881ad206907
Louis <karpx033@umn.edu>
2022-03-07 17:57:59 -0600

    added a transparent background png image to the homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import '../styles/home.css';
+import farmer from '../images/farmanimals.png';
 
 
 export default function Cards() {
 
     return (
         <div className="HomeBackGround">
-            <div className="heightening"></div>
+            <div className="heightening">
+                <h1>Click Us</h1>
+                <img className="homeIMG" src={farmer}></img>
+            </div>
         </div>
     )
 }
