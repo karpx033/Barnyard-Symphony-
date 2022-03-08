07a584e07505a7da66eedae44578bcfa8176b8e6
Louis <karpx033@umn.edu>
2022-03-07 18:10:29 -0600

    routed homepage to a demo page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import '../styles/home.css';
 import farmer from '../images/farmanimals.png';
 
@@ -9,7 +10,9 @@ export default function Cards() {
         <div className="HomeBackGround">
             <div className="heightening">
                 <h1>Click Us</h1>
+                <Link to="/demo">
                 <img className="homeIMG" src={farmer}></img>
+                </Link>
             </div>
         </div>
     )
