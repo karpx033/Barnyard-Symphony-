e0ba63dd163e029db46661b2d695907dd31efb3a
Juno Nguyen <junonguyen27@gmail.com>
2022-03-08 11:19:18 -0600

    add to song button works

--- client/src/components/AnimalList/index.js
+++ client/src/components/AnimalList/index.js
@@ -4,9 +4,9 @@ import chickenDemo from '../../media/ChickenDemo.mp4';
 import sheepDemo from '../../media/SheepDemo.mp4';
 import Carousel from 'react-bootstrap/Carousel';
 import '../../styles/animals.css';
-
 import { useMutation } from '@apollo/client';
 import { ADD_ANIMAL } from '../../utils/mutations';
+import Auth from '../../utils/auth';
 
 var cowNoise = function () {
   var cowSounds = document.getElementById("cowSound");
@@ -26,37 +26,19 @@ var sheepNoise = function () {
   sheepSounds.play();
 };
 
-const AnimalList = ({ songStrings, title }) => {
+
+const AnimalList = ({ songStrings }) => {
 
   const [addAnimal, { error }] = useMutation(ADD_ANIMAL);
 
-  const handleFormSubmit = async (event) => {
-    event.preventDefault();
-
-    const id = document.getElementById('id');
-
-    // On form submit, perform mutation and pass in form data object as arguments
-    // It is important that the object fields are match the defined parameters in `ADD_THOUGHT` mutation
-    try {
-      const { data } = addAnimal({
-        variables: { songStringId: id.value },
-      });
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  if (!songStrings.length) {
-    return <h3>No Animals Yet</h3>;
-  }
-  const cardDiv = {
-    width: "18rem",
-    margin: "10px",
-  }
   const buttonSpace = {
     marginLeft: "5px",
     marginRight: "5px",
-  }
+  };
+
+  if (!songStrings.length) {
+    return <h3>No Animals Yet</h3>
+  };
 
   return (
     <div className="animalsbackground heightening">
@@ -64,7 +46,7 @@ const AnimalList = ({ songStrings, title }) => {
         {songStrings &&
           songStrings.map((songString) => (
 
-            <Carousel.Item>
+            <Carousel.Item key={songString._id}>
 
               <img className="smallIMG" src={songString.img} alt="test"></img>
               <h5 className='centerTxt'>{songString.name}</h5>
@@ -79,7 +61,7 @@ const AnimalList = ({ songStrings, title }) => {
                 {songString.name === "Sheep" &&
                   <button type="button" className="btn animal btn btn-primary" onClick={sheepNoise} style={buttonSpace}>Play Sound</button>
                 }
-                <button type="button" className="btn btn-primary" style={buttonSpace}>Add to Song</button>
+                <button type="button" className="btn btn-primary" style={buttonSpace} onClick={() => { addAnimal( { variables : { songStringId : songString._id , id: Auth.getProfile().data._id } } ) } }>Add to Song</button>
               </div>
               <audio src={cowDemo} id="cowSound"> Your browser does not support the <code>audio</code> element.</audio>
               <audio src={chickenDemo} id="chickenSound"> Your browser does not support the <code>audio</code> element.</audio>
@@ -88,31 +70,6 @@ const AnimalList = ({ songStrings, title }) => {
             </Carousel.Item>
           ))}
       </Carousel>
-
-
-      {/* {songStrings &&
-        songStrings.map((songString) => (
-          <div className="card" style={cardDiv} value={songString._id} key={songString._id} id="id">
-            <img className="card-img-top" src={songString.img} alt="test"></img>
-            <div className="card-body">
-              <h5 className="card-title">{songString.name}</h5>
-              <p className="card-text">{songString.description}</p>
-              {songString.name === "Cow" &&
-                <button type="button" class="btn btn-primary" onClick={cowNoise} style={buttonSpace}>Play Sound</button>
-              }
-              {songString.name === "Chicken" &&
-                <button type="button" class="btn btn-primary" onClick={chickenNoise} style={buttonSpace}>Play Sound</button>
-              }
-              {songString.name === "Sheep" &&
-                <button type="button" class="btn btn-primary" onClick={sheepNoise} style={buttonSpace}>Play Sound</button>
-              }
-              <button type="button" className="btn btn-primary" style={buttonSpace} onClick={handleFormSubmit}>Add to Song</button>
-              <audio src={cowDemo} id="cowSound"> Your browser does not support the <code>audio</code> element.</audio>
-              <audio src={chickenDemo} id="chickenSound"> Your browser does not support the <code>audio</code> element.</audio>
-              <audio src={sheepDemo} id="sheepSound"> Your browser does not support the <code>audio</code> element.</audio>
-            </div>
-          </div>
-        ))} */}
     </div>
   );
 };
