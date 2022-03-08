22bb86a42a4ef786a15521d2f46dbf0e69bad73a
Louis <karpx033@umn.edu>
2022-03-08 14:57:45 -0600

    Yellow title

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -3,16 +3,16 @@ import { Link } from 'react-router-dom';
 import '../styles/home.css';
 import farmer from '../images/farmanimals.png';
 
-import Auth from '../utils/auth';
+
 
 
 export default function Cards() {
-    console.log(Auth.getProfile().data._id)
+    
 
     return (
         <div className="HomeBackGround">
             <div className="heightening">
-                <h1>Click Us</h1>
+                <h1 className='animaltitle'>Click Us</h1>
                 <Link to="/demo">
                 <img className="homeIMG" src={farmer} alt={"farmer"}></img>
                 </Link>
